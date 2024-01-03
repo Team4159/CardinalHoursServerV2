e6d54547dd0d4b5c8eef06df1831a726c7725f05
Calvin Lu <calvinlu@live.com>
2024-01-03 02:24:27 -0800

    Add basic database accessor and unit test

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,30 +1,11 @@
 import mysql from "mysql2";
-const asyncRedis = require("async-redis");
 
-const { MysqlRedisAsync, HashTypes, Caching } = require("mysql-redis");
+const poolPromise = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+}).promise();
 
-const cacheOptions = {
-    expire: 2629746,
-    keyPrefix: "sql.",
-    hashType: HashTypes.md5,
-    caching: Caching.CACHE,
-};
+export default poolPromise;
 
-const redis = asyncRedis.createClient(cacheOptions);
-
-const poolPromise = mysql
-    .createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-    })
-    .promise();
-
-const mysqlRedis = new MysqlRedisAsync(poolPromise, redis);
-
-export default {
-    hashTypes: HashTypes,
-    caching: Caching,
-    db: mysqlRedis,
-};
---
+++ src/tests/database/database.spec.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { describe, it } from "mocha";
+import { expect } from "chai"; // DOES NOT WORK WITH chai v5+ YET BECAUSE USING COMMONJS (https://github.com/chaijs/chai/issues/1561)
+
+import db from "../../database/database";
+
+const getUsers = "SELECT id, firstName, lastName, password, signedIn, lastTime FROM users";
+
+describe("SQL Database Accessor", () => {
+    it("should be connected to database", async () => {
+        const response: any = await db.query("SHOW STATUS WHERE `variable_name` = 'Threads_running'") // See how many threads are currently running
+        const threadsRunning = response[0][0]["Value"];
+
+        expect(threadsRunning).to.be.a("string");
+        expect(Number(threadsRunning)).to.be.greaterThanOrEqual(1);
+    });
+});
