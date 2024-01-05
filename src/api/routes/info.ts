46ea9a49f4403793f63691264eec5fc8f6dd2cef
Calvin Lu <calvinlu@live.com>
2024-01-04 22:34:48 -0800

    Create layout for info.ts

--- src/api/routes/info.ts
+++ src/api/routes/info.ts
@@ -2,12 +2,18 @@ import express from "express";
 
 const router = express.Router();
 
+// Return all users
 router.get("/users", (req, res) => {
-
+    // Return user_id, first_name, last_name, signed_in, last_signed_in, total_time for all users
 });
 
+// Return user given valid ID
 router.get("/users/:id", (req, res) => {
     // req.params.id
+
+    // Is user ID valid?
+
+    // Return user_id, first_name, last_name, signed_in, last_signed_in, total_time of given user
 });
 
 export default router;
